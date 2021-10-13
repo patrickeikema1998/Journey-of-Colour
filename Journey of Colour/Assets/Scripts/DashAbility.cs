0a5c50297a314083a6b112b59688f4492fb3e51a
hoodiefabio <73169440+hoodiefabio@users.noreply.github.com>
2021-10-13 10:44:11 +0200

    Smooth dash
    works with addforce (physics)
    next i'll try doing it by changing the position.

--- Journey of Colour/Assets/Scripts/DashAbility.cs
+++ Journey of Colour/Assets/Scripts/DashAbility.cs
@@ -4,38 +4,45 @@ using UnityEngine;
 
 public class DashAbility : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float dashForce = 0;
-    public float coolDownTime = 0.2f;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float dashForce = 10;
+    [SerializeField] float coolDownTime = 0.2f;
+    [SerializeField] float durationTime = 0.25f;
+    [SerializeField] Material material;
 
     private float direction;
     private float coolDown;
+    private float duration;
 
     void Start()
     {
         coolDown = coolDownTime;
+        duration = 0;
         direction = 1;
     }
     // Update is called once per frame
     void Update()
     {
         coolDown -= Time.deltaTime;
+        duration -= Time.deltaTime;
     }
     void FixedUpdate()
     {
         if (Input.GetAxis("Horizontal") < 0) direction = -1;
         if (Input.GetAxis("Horizontal") > 0) direction = 1;
 
-        if (Input.GetKey(KeyCode.Q) && coolDown < 0)
+        if (Input.GetKey(KeyCode.W) && coolDown < 0 && this.material.color == Color.white)
         {
-            Dash();
+            duration = durationTime;
         }
+
+        if (duration > 0) Dash();
     }
 
     void Dash()
     {
-        coolDown = coolDownTime;
         
-        rb.AddForce(new Vector3(direction*dashForce*400,10,0));
+        rb.AddRelativeForce(new Vector3(direction*dashForce*100,0,0));
+        coolDown = coolDownTime;
     }
 }
