2322987989edf2130aa6204326ecc1f384994e1d
Reno Bisschop <57941435+Reno2301@users.noreply.github.com>
2021-10-22 14:56:17 +0200

    Update PlayerMovement.cs

--- Journey of Colour/Assets/Scripts/Player/PlayerMovement.cs
+++ Journey of Colour/Assets/Scripts/Player/PlayerMovement.cs
@@ -15,9 +15,9 @@ public class PlayerMovement : MonoBehaviour
     public bool isJumpButtonPressed = false;
     public bool isGrounded = false;
 
-    public bool lookingLeft;
+    bool lookingLeft;
 
-    public string lastPressed;
+    string lastPressed;
     string currentPressed;
 
     public GameObject bullet;
