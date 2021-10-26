53005e272ef5f40e917fa316e4b120be9624269e
YoloJordy <71704175+YoloJordy@users.noreply.github.com>
2021-10-26 12:13:16 +0200

    Finished slime boss

--- Journey of Colour/Assets/Scripts/Boss/SlimeBossController.cs
+++ Journey of Colour/Assets/Scripts/Boss/SlimeBossController.cs
@@ -1,33 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SlimeBossController : MonoBehaviour
 {
     Rigidbody m_Rigidbody;
+    Health health;
+    EnemyShoot rangedAttack;
 
     bool onGround;
+    int phase = 1;
 
-    [SerializeField] float jumpCooldown = 1;
+    [SerializeField] int meleeDamage = 4;
+
+    [SerializeField] float jumpCooldownPhase1 = 1, jumpCooldownPhase2 = 1f, jumpCooldownPhase3 = 0.5f,
+                           shootCooldownPhase2 = 2, shootCooldownPhase3 = 1,
+                           burstTime = 1.6f;
+    float jumpCooldown;
     float jumpCooldownTimer = 0;
 
     [SerializeField] float jumpForce = 10,
                            sideForce = 5;
+
 
     bool facingLeft = true;
     // Start is called before the first frame update
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        health = GetComponent<Health>();
+        rangedAttack = GetComponent<EnemyShoot>();
+        SwitchPhase(1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //jumpen doet hij altijd de rest hang af van welke phase
         if (onGround)
         {
             jumpCooldownTimer += Time.deltaTime;
             if (jumpCooldownTimer >= jumpCooldown) Jump();
+        }
+        switch (phase)
+        {
+            case 1:
+                if (health.GetHealth < health.maxHealth / 3 * 2) SwitchPhase(phase + 1);  
+                break;
+            case 2:
+                if (health.GetHealth < health.maxHealth / 3) SwitchPhase(phase + 1);
+                break;
+            case 3:
+                if (health.dead) /*death sequence*/Destroy(gameObject);
+                break;
         }
         
     }
@@ -53,10 +76,43 @@ public class SlimeBossController : MonoBehaviour
             onGround = true;
             jumpCooldownTimer = 0;
         }
-
-        if (collision.gameObject.name.StartsWith("SideWall"))
+        else if (collision.gameObject.CompareTag("Player"))
         {
-            facingLeft = !facingLeft;
+            collision.gameObject.GetComponent<Health>().Damage(meleeDamage);
         }
+        if (collision.gameObject.name.StartsWith("SideWall")) facingLeft = !facingLeft; 
+        
+    }
+
+    void SwitchPhase(int newPhase)
+    {
+        switch (newPhase)
+        {
+            case 1:
+                rangedAttack.enabled = false;
+                jumpCooldown = jumpCooldownPhase1;
+                break;
+            case 2:
+                InvokeRepeating("InvokeRangedAttack", shootCooldownPhase2, shootCooldownPhase2 + burstTime);
+                jumpCooldown = jumpCooldownPhase2;
+                break;
+            case 3:
+                CancelInvoke("InvokeRangedAttack");
+                EndRangedAttack();
+                InvokeRepeating("InvokeRangedAttack", shootCooldownPhase3, shootCooldownPhase3 + burstTime);
+                jumpCooldown = jumpCooldownPhase3;
+                break;
+        }
+        phase = newPhase;
+    }
+    void InvokeRangedAttack()
+    {
+        rangedAttack.enabled = true;
+        Invoke("EndRangedAttack", burstTime);
+    }
+
+    void EndRangedAttack()
+    {
+        rangedAttack.enabled = false;
     }
 }
