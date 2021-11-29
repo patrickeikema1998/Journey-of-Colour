5415e8dd113fe13fbb04b079d0de4d08ea828a9d
Yukichabo <chantal@boodt.eu>
2021-11-29 16:00:42 +0100

    smoother Rotation + Counterfix

--- Journey of Colour/Assets/Scripts/Player/Counter.cs
+++ Journey of Colour/Assets/Scripts/Player/Counter.cs
@@ -5,76 +5,119 @@ using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
-    [SerializeField] float bulletForce, enemyForce, cooldownTime;
+    [SerializeField] float bulletForce, enemyForce, counterDistance;
     private bool keyDown;
     private CharacterController controller;
-    private CustomTimer cooldownTimer;
     private SwapClass swapClass;
+
+    private GameObject player;
+    private float distance;
     //https://www.youtube.com/watch?v=_w7GU2NIxUE
     //https://answers.unity.com/questions/1100879/push-object-in-opposite-direction-of-collision.html
 
     private void Start()
     {
-        cooldownTimer = new CustomTimer(cooldownTime);
-        cooldownTimer.finish = true;
-
-        swapClass = GetComponent<SwapClass>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        swapClass = player.GetComponent<SwapClass>();
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T)) 
-        { 
+        if (Input.GetKeyDown(KeyCode.W))
+        {
             keyDown = true;
-            cooldownTimer.Reset();
-            cooldownTimer.start = true;
         }
 
-        if (cooldownTimer.finish) { keyDown = false; }
-        cooldownTimer.Update();
+        distance = Vector3.Distance(transform.position, player.transform.position);
+        if (keyDown) { Redirect(); }
     }
 
-    void OnTriggerEnter(Collider c)
+    void Redirect()
     {
-        if (keyDown && swapClass.currentClass == SwapClass.playerClasses.Devil)
+
+        if (keyDown && swapClass.currentClass == SwapClass.playerClasses.Devil && distance <= counterDistance)
         {
-            Vector3 dir = (c.gameObject.transform.position - transform.position).normalized;
+            Vector3 dir = (transform.position - player.transform.position).normalized;
 
             // If the object we hit is the enemy
-            if (c.gameObject.tag == "Enemy")
+            if (gameObject.tag == "Enemy")
             {
-                float force = enemyForce;
-
-                if (c.gameObject.GetComponent<Rigidbody>() == null) 
+                if (GetComponent<Rigidbody>() == null)
                 {
-                    this.controller = c.GetComponent<EnemyController>().controller;
+                    this.controller = GetComponent<EnemyController>().controller;
                     if (controller != null)
                     {
-                        c.gameObject.transform.forward = (new Vector3(
+                        transform.forward = (new Vector3(
                         dir.x,
                         dir.y,
                         dir.z)
                         );
-                        controller.SimpleMove(c.gameObject.transform.forward * enemyForce);
+
+                        controller.SimpleMove(transform.forward * enemyForce);
                     }
                 }
                 else
                 {
-                    c.gameObject.GetComponent<Rigidbody>().AddForce(dir * enemyForce, ForceMode.Impulse);
+                    //works only because the enemy is taller than the player
+                    //y and z are switched on purpose (enemy does not move)
+                    dir = new Vector3(dir.x, dir.z, dir.y);
+
+                    GetComponent<Rigidbody>().AddForce(dir * enemyForce, ForceMode.Impulse);
                 }
 
             }
 
             // If the object we hit is a bullet
-            if (c.gameObject.tag == "Bullet")
+            if (gameObject.tag == "Bullet")
             {
-                float force = bulletForce;
-                c.gameObject.GetComponent<Rigidbody>().AddForce(dir * bulletForce, ForceMode.Impulse);
+                GetComponent<Rigidbody>().AddForce(dir * bulletForce, ForceMode.Impulse);
             }
-
             keyDown = false;
         }
     }
+    //void OnTriggerEnter(Collider c)
+    //{
+    //    Debug.Log("SAVE ME");
+    //    if (keyDown && swapClass.currentClass == SwapClass.playerClasses.Devil)
+    //    {
+    //        Vector3 dir = (c.gameObject.transform.position - transform.position).normalized;
+
+
+    //        // If the object we hit is the enemy
+    //        if (c.gameObject.tag == "Enemy")
+    //        {
+    //            float force = enemyForce;
+
+    //            if (c.gameObject.GetComponent<Rigidbody>() == null)
+    //            {
+    //                this.controller = c.GetComponent<EnemyController>().controller;
+    //                if (controller != null)
+    //                {
+    //                    c.gameObject.transform.forward = (new Vector3(
+    //                    dir.x,
+    //                    dir.y,
+    //                    dir.z)
+    //                    );
+    //                    controller.SimpleMove(c.gameObject.transform.forward * enemyForce);
+    //                }
+    //            }
+    //            else
+    //            {
+    //                c.gameObject.GetComponent<Rigidbody>().AddForce(dir * enemyForce, ForceMode.Impulse);
+    //            }
+
+    //        }
+
+    //        // If the object we hit is a bullet
+    //        if (c.gameObject.tag == "Bullet")
+    //        {
+    //            float force = bulletForce;
+    //            c.gameObject.GetComponent<Rigidbody>().AddForce(dir * bulletForce, ForceMode.Impulse);
+    //        }
+
+    //        keyDown = false;
+    //    }
+    //}
 }
 //// If the object we hit is the enemy
 //if (c.gameObject.tag == "Enemy")
