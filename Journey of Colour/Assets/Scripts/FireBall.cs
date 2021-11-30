0549779f5580df7cfb4cd00646d0845bc0f8e4d0
Reno Bisschop <57941435+Reno2301@users.noreply.github.com>
2021-11-30 21:11:29 +0100

    FireBall Tweaking
    You have a cooldown when shooting a fireball and the fireball will be destroyed when it's outside the screen.

--- Journey of Colour/Assets/Scripts/FireBall.cs
+++ Journey of Colour/Assets/Scripts/FireBall.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class FireBall : MonoBehaviour
 {
     public float speed = 20;
-    public Rigidbody rb;
+    float outOfRange = 60;
 
+    public Rigidbody rb;
     public GameObject player;
     private PlayerMovement playerMovement;
 
@@ -18,6 +19,15 @@ public class FireBall : MonoBehaviour
         rb.velocity = transform.right * speed;
     }
 
+    // When this gameObject is ouside the screen, it will be destroyed.
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    // If this gameObject collides with an object with the tag "Enemy", the
+    // enemy will take 5 damage from within the HealthClass on the enemy itself.
+    // If it collides with something else, it will simply get destroyed
     void OnTriggerEnter(Collider collision)
     {
         if (collision.tag == "Enemy")
--- Journey of Colour/Assets/Scripts/Player/Weapon.cs
+++ Journey of Colour/Assets/Scripts/Player/Weapon.cs
@@ -7,23 +7,29 @@ public class Weapon : MonoBehaviour
     public Transform fireBallPointLeft;
     public Transform fireBallPointRight;
     public GameObject fireBallPrefab;
+    public float shootTime;
 
     SwapClass swapClass;
     PlayerMovement playerMovement;
+    CustomTimer shootTimer;
 
     private void Start()
     {
         swapClass = GetComponent<SwapClass>();
         playerMovement = GetComponent<PlayerMovement>();
+        shootTimer = new CustomTimer(shootTime);
+        shootTimer.start = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        shootTimer.Update();
+
         //player can only fire a fireball when he is in class 1. also known as the black colour.
         if (swapClass.playerClass == 1)
         {
-            if (Input.GetMouseButtonDown(1))
+            if (Input.GetMouseButtonDown(1) && shootTimer.finish == true)
             {
                 if (!playerMovement.lookingLeft)
                 {
@@ -33,6 +39,8 @@ public class Weapon : MonoBehaviour
                 {
                     ShootLeft();
                 }
+                shootTimer.Reset();
+                shootTimer.start = true;
             }
         }
     }
