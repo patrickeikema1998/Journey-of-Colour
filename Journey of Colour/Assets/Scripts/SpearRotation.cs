a22441a6dd29afee2004e4ec8e2e47769b766835
patrickeikema <patrick.eikema@kpnmail.nl>
2021-12-14 17:32:41 +0100

    found a new landscape, this ones better and smaller disk space

--- Journey of Colour/Assets/Scripts/SpearRotation.cs
+++ Journey of Colour/Assets/Scripts/SpearRotation.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpearRotation : MonoBehaviour
 {
     Rigidbody rb;
-    float divider = 15f;
+    float divider = 20f;
     float angle;
     float startRotationZ, downRotationZ;
     Vector3 force;
