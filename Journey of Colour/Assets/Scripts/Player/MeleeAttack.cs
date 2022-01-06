18cf69cf9f724164ad667de35999991d6e4708f5
YoloJordy <71704175+YoloJordy@users.noreply.github.com>
2022-01-06 11:41:43 +0100

    Changed position of player attackbox

--- Journey of Colour/Assets/Scripts/Player/MeleeAttack.cs
+++ Journey of Colour/Assets/Scripts/Player/MeleeAttack.cs
@@ -7,6 +7,7 @@ public class MeleeAttack : MonoBehaviour
     [SerializeField] int damage;
     [SerializeField] float attackOffset = 1, attackRange = 1;
     [SerializeField] LayerMask opponentLayer;
+    [SerializeField] Vector3 centerOffset = Vector3.zero;
     Vector3 attackBox;
 
     // Start is called before the first frame update
@@ -16,22 +17,20 @@ public class MeleeAttack : MonoBehaviour
         attackBox = new Vector3(attackRange / 2, attackRange / 4, attackRange / 2);
     }
 
-    // Update is called once per frame
-    void Update()
+    Vector3 BoxCenter
     {
-        Debug.DrawLine(transform.position + (transform.forward * attackOffset), transform.position + (transform.forward * attackOffset) + (transform.forward * attackBox.x));
+        get { return transform.position + (transform.forward * attackOffset) + centerOffset; }
     }
 
     public void Attack()
     {
         //maakt een array van alle colliders binnen de attackRange en als deze een health component hebben word er health afgehaald
         Collider[] overlaps;
-        overlaps = Physics.OverlapBox(transform.position + (transform.forward * attackOffset), attackBox, transform.rotation, opponentLayer);
+        overlaps = Physics.OverlapBox(BoxCenter, attackBox, transform.rotation, opponentLayer);
 
         foreach (Collider opponent in overlaps)
         {
             if (opponent.GetComponent<Health>() != null) opponent.GetComponent<Health>().Damage(damage);
         }
-
     }
 }
