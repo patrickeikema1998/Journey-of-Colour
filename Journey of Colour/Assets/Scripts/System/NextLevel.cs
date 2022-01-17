70feb8198f3d955deff102d8ae28c1e0c6cd95d5
hoodiefabio <73169440+hoodiefabio@users.noreply.github.com>
2022-01-17 18:08:14 +0100

    Complete Level finish
    Able to transition to scenes & able to beat levels and move to next level.

--- Journey of Colour/Assets/Scripts/System/NextLevel.cs
+++ Journey of Colour/Assets/Scripts/System/NextLevel.cs
@@ -5,25 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
+    [SerializeField] GameObject winScreen;
     Scene currentScene;
     int nextSceneIndex;
+    bool levelBeat = false;
 
     // Start is called before the first frame update
     void Start()
     {
         currentScene = SceneManager.GetActiveScene();
         nextSceneIndex = currentScene.buildIndex + 1;
+        winScreen.SetActive(false);
+    }
+
+    
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            levelBeat = true;
+            winScreen.SetActive(true);
+        }
     }
 
     // Update is called once per frame
-    private void OnCollisionEnter(Collision collision)
+
+    private void Update()
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (levelBeat == true && Input.GetKeyDown(KeyCode.Space))
         {
             GoToNextLevel();
         }
     }
-
     public void GoToNextLevel()
     {
         if (nextSceneIndex > SceneManager.sceneCount)
